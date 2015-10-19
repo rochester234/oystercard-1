3e4a05ef3d0ea26adfad80998d836f4d56498817
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-19 18:00:39 +0100

    Added top_up by amount test (passed)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,14 @@
 class Oystercard
-  
-  attr_reader :balance
 
-  def initialize
-  @balance=0
+  attr_accessor :balance
+  DEFAULT_BALANCE = 0
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def top_up(amount)
+    @balance += amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,18 @@ require 'oystercard'
 
 describe Oystercard do
   it "has a balance of zero" do
-    card = Oystercard.new
     expect(subject.balance).to eq (0)
   end
+
+describe '#top_up' do
+  it "responds to top_up" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it "tops up by an amount" do
+    expect{subject.top_up 1}.to change{subject.balance}.by 1
+  end
+end
 
 
 end
