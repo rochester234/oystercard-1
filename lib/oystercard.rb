deebd4b0b0be1381098002179df581eed7a67199
mongolianprincess <ayc.yang@gmail.com>
2015-10-19 18:31:17 +0100

    Added maximum limit to balance, ch 06

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,14 @@ class Oystercard
 
   attr_accessor :balance
   DEFAULT_BALANCE = 0
+  BALANCE_LIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(amount)
+    fail "The limit is 90" if amount + balance > BALANCE_LIMIT
     @balance += amount
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  let (:oystercard) {Oystercard.new}
   it "has a balance of zero" do
     expect(subject.balance).to eq (0)
   end
@@ -13,7 +14,12 @@ describe '#top_up' do
   it "tops up by an amount" do
     expect{subject.top_up 1}.to change{subject.balance}.by 1
   end
-end
 
+  it "has a maximum balance at 90" do
+    max_balance = Oystercard::BALANCE_LIMIT
+    subject.top_up(max_balance)
+    expect{subject.top_up 1}.to raise_error "The limit is 90"
+  end
+end
 
 end
